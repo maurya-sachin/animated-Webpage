45dabe354f1d337b6d25d126e117534ef7dcd8a2
Sachin <sachinmaurya1710@gmail.com>
2023-08-28 14:32:51 +0530

    code for cursor animation

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var cursr = document.querySelector("#cursor")
+var cursrbck = document.querySelector("#cursorbck")
+document.addEventListener("mousemove", function (position) {
+    cursr.style.left = position.x + "px"
+    cursr.style.top = position.y + "px"
+    cursrbck.style.left = position.x - 225 + "px"
+    cursrbck.style.top = position.y - 225 + "px"
+})
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     height: "100px",
@@ -6,20 +15,20 @@ gsap.to("#nav", {
         trigger: "#nav",
         scroller: "body",
         // markers:true,
-        start:"top -10%",
-        end:"top -11%",
-        scrub:1
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
     }
 })
 
-gsap.to("#main",{
-    backgroundColor:"#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
         // markers:true,
-        start:"top -25%",
-        end:"top -75%",
-        scrub:2
+        start: "top -25%",
+        end: "top -75%",
+        scrub: 2
     }
 })
