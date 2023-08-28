3e3a77a7033b6578a89cb3dcee98443a515ac1a7
Sachin <sachinmaurya1710@gmail.com>
2023-08-28 14:00:57 +0530

    coded gsap for page 2 landing animation

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ gsap.to("#nav", {
     scrollTrigger: {
         trigger: "#nav",
         scroller: "body",
-        markers:true,
+        // markers:true,
         start:"top -10%",
         end:"top -11%",
         scrub:1
     }
 })
 
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -75%",
+        scrub:2
+    }
+})
